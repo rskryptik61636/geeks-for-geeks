53a74c659ae91783b2ad514f28670746ed876922
rskryptik61636 <mk.rohit.shrinath@gmail.com>
2024-05-11 20:44:02 -0700

    Add towers of hanoi to number-problems

--- number-problems/number-problems.cpp
+++ number-problems/number-problems.cpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <algorithm>
 #include <unordered_set>
+#include <stack>
 
 using namespace std;
 
@@ -450,6 +451,45 @@ int getPairsCount(int arr[], int n, int k)
             s.insert(k - arr[i]);
     }
     return count;
+}
+
+// Towers of Hanoi: https://www.geeksforgeeks.org/iterative-tower-of-hanoi/
+char rod[] = { 'S', 'A', 'D' };
+vector<stack<int>> stacks(3); // 3 stacks for 3 rods
+
+void moveDisk(int a, int b)
+{
+    if (stacks[b].empty() || (!stacks[a].empty() && stacks[a].top() < stacks[b].top()))
+    {
+        cout << "Move disk " << stacks[a].top() << " from rod " << rod[a] << " to rod " << rod[b] << "\n";
+        stacks[b].push(stacks[a].top());
+        stacks[a].pop();
+    }
+    else
+        moveDisk(b, a);
+}
+
+void towerOfHanoi(int n)
+{
+    cout << "Tower of Hanoi for " << n << " disks:\n";
+
+    int src = 0, aux = 1, dest = 2;
+    for (int i = n; i > 0; i--)
+        stacks[src].push(i);
+
+    int totalMoves = (1 << n) - 1;
+    if (n % 2 == 0)
+        swap(aux, dest);
+
+    for (int i = 1; i <= totalMoves; i++)
+    {
+        if (i % 3 == 0)
+            moveDisk(aux, dest);
+        else if (i % 3 == 1)
+            moveDisk(src, dest);
+        else
+            moveDisk(src, aux);
+    }
 }
 
 int main( int argc, char* argv[] )
