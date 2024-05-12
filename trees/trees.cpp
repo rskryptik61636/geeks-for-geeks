0beab1f039804bd5c7dd944ce0b6a8a46eecb1ed
rskryptik61636 <mk.rohit.shrinath@gmail.com>
2024-05-12 00:29:36 -0700

    Add sortedArrayToBST

--- trees/trees.cpp
+++ trees/trees.cpp
@@ -1,5 +1,8 @@
 #include <vector>
+#include <queue>
 #include <cmath>
+
+using namespace std;
 
 // Get left\right children indices
 std::size_t left(const std::size_t i) { return 2 * i + 1; }
@@ -49,6 +52,101 @@ void heapSort(std::vector<int>& data)
     }
 }
 
+// function to convert the array to BST
+// and return the root of the created tree
+vector<size_t> sortedArrayToBST(vector<size_t>& nums)
+{
+    // if the array is empty return NULL
+    if (nums.empty()) {
+        return vector<size_t>();
+    }
+
+    constexpr size_t nullIndx = -1;
+    auto n = nums.size();
+
+    /* Algorithm:
+    * - Pick mid value of sorted array as root
+    * - Left child = midpoint of left half; Right child = midpoint of right half
+    * - RootIndx = 0. Set root, left and right children in result.
+    *
+    * Sample traversal:
+    * 1, 2, 3, 4, 5, 6, 7
+
+        result: -1, -1, -1, -1, -1, -1, -1
+        root: 4, lc: 2, rc: 6
+        result: 4, 2, 6, -1, -1, -1, -1
+        queue: {(indx1, {indx0, indxNULL}), (indx5, {indx4, indx6})}
+
+        curr = (indx1, {indx0, indxNULL})
+        root: 2, lc: 1, rc: 3
+        result: 4, 2, 6, 1, 3, -1, -1
+            No subtree for indx0, skip
+
+        curr = (indx5, {indx4, indx6})
+        root: 6, lc: 5, rc: 7
+        result: 4, 2, 6, 1, 3, 5, 7
+    */
+
+    // BST in array form
+    // Left child: 2*i + 1
+    // Right child: 2*i + 2
+    vector<size_t> result(n, nullIndx);
+    auto fnGetTargetLeftChildIndx = [](const size_t i) { return 2 * i + 1; };
+    auto fnGetTargetRightChildIndx = [](const size_t i) { return 2 * i + 2; };
+    struct Node
+    {
+        size_t dstParentIndx, start, end;
+    };
+    queue<Node> traversal;
+
+    // Traversal update function
+    auto fnUpdateTraversal = [&](const size_t dstParentIndx, const size_t left, const size_t right)
+    {
+        // Proceed only if left != right
+        if (left != right)
+        {
+            auto mid = (left + right) >> 1;
+            auto srcParentIndx = mid;
+            auto srcLeftChildIndx = (left + mid - 1) >> 1;
+            auto srcRightChildIndx = (mid + 1 + right) >> 1;
+
+            auto dstLeftChildIndx = fnGetTargetLeftChildIndx(dstParentIndx);
+            auto dstRightChildIndx = fnGetTargetRightChildIndx(dstParentIndx);
+
+            result[dstParentIndx] = nums[srcParentIndx];
+            result[dstLeftChildIndx] = nums[srcLeftChildIndx];
+            result[dstRightChildIndx] = nums[srcRightChildIndx];
+
+            // Push left child subtree only if valid
+            if (left != srcParentIndx - 1)
+            {
+                traversal.push({ dstLeftChildIndx, left, srcParentIndx - 1 });
+            }
+
+            if (right != srcParentIndx + 1)
+            {
+                traversal.push({ dstRightChildIndx, srcParentIndx + 1, right });
+            }
+        }
+    };
+
+    // Mid point as root
+    auto start = 0;
+    auto end = n - 1;
+    fnUpdateTraversal(0, start, end);
+
+    // Traverse subtrees until complete.
+    while (!traversal.empty())
+    {
+        auto currNode = traversal.front();
+        traversal.pop();
+
+        fnUpdateTraversal(currNode.dstParentIndx, currNode.start, currNode.end);
+    }
+
+    return result;
+}
+
 int main()
 {
     //// Test maxHeapify
@@ -59,9 +157,13 @@ int main()
     //std::vector<int> data{ 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
     //buildMaxHeap(data);
 
-    // Test heap sort
-    std::vector<int> data{ 16, 4, 10, 14, 7, 9, 3, 2, 8, 1 };
-    heapSort(data);
+    //// Test heap sort
+    //std::vector<int> data{ 16, 4, 10, 14, 7, 9, 3, 2, 8, 1 };
+    //heapSort(data);
+
+    // Test sortedArrayToBst
+    vector<size_t> nums = { 1, 2, 3, 4, 5, 6, 7 };
+    auto bst = sortedArrayToBST(nums);
 
     return 0;
 }
